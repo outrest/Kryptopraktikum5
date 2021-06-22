c56cd7bc2f507161c850ff560abfd7e2333e4606
outrest <florianm@onlinehome.de>
2021-06-22 09:01:04 +0200

    ES FUNKTIONIERT!

--- src/Aufgabe2.java
+++ src/Aufgabe2.java
@@ -13,40 +13,43 @@ public class Aufgabe2 {
 	BigInteger p;
 	BigInteger q;
 	BigInteger d;
-	int n;
+	BigInteger n;
 
 	public void ausfuehren() {
 		System.out.println(
 				"-----------------------------------------------------------------------------\nFühre Aufgabe 2 "
-				+ "aus:\n\n");
+				+ "aus:\nErgebnis: ");
 
 		//1.
 		BigInteger xp = x.mod(p);
 		BigInteger xq = x.mod(q);
 		BigInteger dp = d.mod(p.subtract(BigInteger.ONE));
 		BigInteger dq = d.mod(q.subtract(BigInteger.ONE));
+		//RICHTIG
 
 		//2.
 		BigInteger yp = xp.modPow(dp, p);
 		BigInteger yq = xq.modPow(dq, q);
+		boolean gleich = yp.compareTo(yq) == 0;
+		System.out.println("Yp und Yq sind gleich: " + gleich);
 
 		//3.
-		BigInteger cp = q.modInverse(p);
-		BigInteger cq = p.modInverse(q);
+		BigInteger cp = q.modPow(BigInteger.ZERO.subtract(BigInteger.ONE), p);
+		BigInteger cq = p.modPow(BigInteger.ZERO.subtract(BigInteger.ONE), q);
 
-		BigInteger y =
-				(q.multiply(cp).multiply(yp).add(p.multiply(cq)
-				 .multiply(yq))).mod(BigInteger.valueOf(n));
+		BigInteger ylinks = q.multiply(cp).multiply(yp);
+		BigInteger yrechts = p.multiply(cq).multiply(yq);
+		BigInteger y = ylinks.add(yrechts).mod(n);
 		System.out.println(y);
 	}
 
 	public Aufgabe2(Aufgabe1 aufgabe1) {
 		this.aufg1 = aufgabe1;
-		x =  aufg1.getVerschluesselung();
+		x = aufg1.getVerschluesselung();
 		p = aufg1.getP();
 		q = aufg1.getQ();
 		d = aufg1.getD();
-		n = aufg1.getNumBits();
+		n = aufg1.getn();
 
 		ausfuehren();
 	}
--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@
 public class Main {
 	public static void main(String[] args) {
 		int klartext = 4711;
-		Aufgabe1 AufgabeEins = new Aufgabe1(1500, klartext);
+		Aufgabe1 AufgabeEins = new Aufgabe1(3096, klartext);
 		Aufgabe2 AufgabeZwei = new Aufgabe2(AufgabeEins);
 	}
 }
